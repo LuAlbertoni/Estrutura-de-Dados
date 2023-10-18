8a82333556976a68d178fb1ba216fea49792e55b
Luís Albertoni <lualbertoni@protonmail.com>
2023-10-18 20:07:49 -0300

    Uploading Files

---
+++ Lista-Linear-Encadeada-(Pilha)/Ex19/Program.cs
@@ -0,0 +1,38 @@
+﻿// Faça uma implementação que construa uma lista encadeada. Seu programa deve inserir vários valores na lista e em seguida removê-los. Após remover um elemento da lista, exiba-o na tela.
+
+tp_no topo = null;
+
+void Insere(int valor)
+{
+    tp_no no = new tp_no();
+    no.valor = valor;
+    no.prox = topo;
+
+    topo = no;
+}
+
+tp_no Remove()
+{
+    tp_no no = topo;
+    if (topo != null)
+        topo = topo.prox;
+
+    return no;
+}
+
+for (int i = 0; i < 5; i++)
+{
+    Console.Write("Informe um valor: ");
+    Insere(Convert.ToInt32(Console.ReadLine()));
+}
+
+Console.WriteLine("\nValores removidos da lista:");
+
+while (topo != null)
+    Console.WriteLine(Remove().valor);
+
+class tp_no
+{
+    public int valor = 0;
+    public tp_no prox = null;
+}
